1e671892e3973a6fa3c2a142db6019871938431d
Jeffrey Neer <admin@jeffreyneer.com>
2015-10-15 18:34:16 -0500

    Added basic serach alg java(not Tested)

--- JavaImplement/src/Main.java
+++ JavaImplement/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
 import java.util.*;
 
 public class Main
@@ -6,6 +9,11 @@ public class Main
 	{
 		letterGen gen = new letterGen();
 		char[] letters = gen.generateChar();
+        try {
+            wordListSearch search = new wordListSearch(new File("somelocation.txt"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
         for(int i = 0;i<letters.length; i++){
             System.out.println(letters[i]);
         }
--- JavaImplement/src/wordListSearch.java
+++ JavaImplement/src/wordListSearch.java
@@ -1,3 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class wordListSearch
 {
+    Scanner scanner;
+    CharSequence comparitor;
+    public wordListSearch(File input) throws FileNotFoundException {
+        scanner = new Scanner(input);
+    }
+    public ArrayList<String> searchList(char[] input){
+        ArrayList<String> outputList = new ArrayList<String>();
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            int counter = 0;
+            for(int i = 0; i < line.length(); i++){
+
+                comparitor = Character.toString(input[i]);
+                if(line.contains(comparitor)){
+                    counter++;
+                }
+            }
+            if(counter==line.length()){
+                outputList.add(line);
+            }
+        }
+        return  outputList;
+    }
+
+
+
 }

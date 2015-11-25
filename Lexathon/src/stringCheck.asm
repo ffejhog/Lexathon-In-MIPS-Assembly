858c9e0e19482e9e0e2130d5cf2f2b299afb889a
Daniel Lecheler <dgl140030@utdallas.edu>
2015-11-24 21:56:36 -0600

    The stringCheck now incorporates the Scoring subroutine by adding the length of the correct input directly to $s3. (The separate Scoring file is obsolete)  The bugs that previously caused errors in detecting correct input have been fixed and the subroutine has been tested successfully. The subroutine also checks for duplicates by using a series of booleans representing whether the word has already been entered by the user, which is represented by the label DuplicateList. The 4 seconds being added to the timer was not due to my code, so otherwise I have addressed all the previous things requested of me so far.

--- Lexathon/src/stringCheck.asm
+++ Lexathon/src/stringCheck.asm
@@ -22,17 +22,16 @@ addi $sp, $sp, -4
 sw $ra, 0($sp)
 
 li $t2, 0	# zeros out $t2 to be prepared for their uses in the primary loop (StrChLoop0)
-
-move $t3, $a0	# copies the address of the input to $t3
+li $t3, 0	# the duplicate sentinal used to determine if a correct word is entered twice
 
 StrChLoop0:	# the primary loop; compares each individual letter of every word
 
 lb $t0, 0($a0)	# loads the current letters of the strings to $t0 and $t1
 lb $t1, 0($a1)	
 
-bne $t0, $t1, StrChReset # compares the current letters, and branches if the letters are different 
-
 beqz $t1, StrChLoop1	# exits the loop if the null terminator is reached in the current correct string
+
+bne $t0, $t1, StrChReset0 # compares the current letters, and branches if the letters are different 
 
 addi $a0, $a0, 1	# increments the current letter of each string, and the size of the string as well stored in $t2
 addi $a1, $a1, 1	
@@ -42,24 +41,25 @@ j StrChLoop0
 
 StrChLoop1:
 
-beqz $t0, StrMatch	# concludes the words are a match if $t0 is also null terminated
+beqz $t0, StrDuplicateCheck	# concludes the words are a match if $t0 is also null terminated, and then sends it to be checked for duplicates
 
 addi $a1, $a1, 1	# increments the current letter of the list of correct strings
 lb $t1, 0($a1)		# loads the letter to $t0
 
 beqz $t1, StrNoMatch	# ends search when double null terminator is reached, and concludes there is no match
 
-sub $a0, $a0, $t2	# resets the user input
-li $t2, 0		# zeroes out the letter counter
-j StrChLoop0		# returns to the primary loop
+StrChReset0:		# if StrChLoop1 finds neither a match or a double null terminator, then StrChReset0 is used automatically
 
-
-StrChReset:
-
+beqz $t1, StrChReset1	# checks if $t1 is already the null terminator, and branches if necissary
 addi $a1, $a1, 1	# increments the letter in the list of correct strings
 lb $t1, 0($a1)		# loads the letter to $t0
 bnez $t1, StrChReset	# runs the loop until the null terminator is reached
-move $a0, $t3		# restes the user input
+
+StrChReset1:
+
+addi $a1, $a1, 1	# increments the letter in the list of correct strings from the null to the start of the next word
+addi $t3, $t3, 1	# increments the current number of the word in the word list
+sub $a0, $a0, $t2	# restes the user input
 li $t2, 0		# zeroes out the counter
 j StrChLoop0		# returns to the primary loop
 
@@ -68,8 +68,7 @@ StrMatch:
 
 move $v0, $t2	# returns the length of the input
 
-move $a0, $t2	# prepares $a0 to be passed to Scoring subroutine
-jal Scoring	
+add $s3, $s3, $t2	# adds the length of the correct word to the total score
 
 lw $ra, 0($sp)
 addi $s0, $s0, 4
@@ -77,6 +76,7 @@ addi $s0, $s0, 4
 jr $ra
 
 StrNoMatch:
+
 li $v0, -1	# returns -1 if there is no match
 
 lw $ra, 0($sp)
@@ -84,4 +84,12 @@ addi $s0, $s0, 4
 
 jr $ra
 
+StrDuplicateCheck:
 
+la $t4, DuplicateList		# assigns $t4 the address of the location in the series of boolean representing which words have been chosen
+add $t4, $t4, $t3		# adds the number of words checked to $t4, to determine the exact word's boolean value
+lb $t5, 0($t4)			# loads the boolean that determines if the word has already been used as an input
+bnez $t5, StrNoMatch		# branches to StrNoMatch if the boolean is not zero
+li $t5, 1			# stores 1 to the current location of the word in the DuplicateList to show it has been used
+sb $t5 0($t4)			
+j StrMatch			# jumps to StrMatch if the boolean was zero
